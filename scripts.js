d90b51b8284fbed20dbf3a8f380b4d75f305c365
Rayon-B <sting.ray.kappa@gmail.com>
2023-09-02 21:06:25 -0400

    Add matches, playround() now in game(rounds)

--- scripts.js
+++ scripts.js
@@ -1,61 +1,155 @@
-// getComputerChoice() 
-// this function needs to return a random choice of rock paper or scissors
-// try researching Math
+// // getComputerChoice() 
+// // this function needs to return a random choice of rock paper or scissors
+// // try researching Math
 
-let optionsRPS = ["Rock", "Paper", "Scissors"]
+// let optionsRPS = ["Rock", "Paper", "Scissors"]
 
-function getComputerChoice() {
-   return optionsRPS[(Math.floor(Math.random() * optionsRPS.length))];
-}
-
-
-
-// playRound()
-// take the user and computers choices and alert the winner
-// steps...
-// it will compare two inputs
-// it will know each win condition 
-// it will display a message on win
+// function getComputerChoice() {
+//    return optionsRPS[(Math.floor(Math.random() * optionsRPS.length))];
+// }
 
 
-const playerSelection = prompt("Pick one: ")
-const computerSelection = getComputerChoice()
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock") {
-        if (computerSelection == "Paper") {
-            alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
+// // // playRound()
+// // // take the user and computers choices and alert the winner
+// // // steps...
+// // // it will compare two inputs
+// // // it will know each win condition 
+// // // it will display a message on win
+
+
+// const playerSelection = prompt("Pick one: ").toLowerCase()
+// const computerSelection = getComputerChoice().toLocaleLowerCase()
+
+// function playRound(playerSelection, computerSelection) {
+
+
+
+//     if (playerSelection == "rock") {
+//         if (computerSelection == "paper") {
+//             alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+//         else if (computerSelection == "scissors") {
+//             alert(`You won! You choose ${playerSelection} vs. ${computerSelection}`);
+//         } 
+//         else {
+//             alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+
+
+
+
+
+//     } else if (playerSelection == "paper"){
+//         if (computerSelection == "rock") {
+//             alert(`You Won! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+//         if (computerSelection == "scissors") {
+//             alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+//         else {
+//             alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+
+
+
+
+
+//     } else if (playerSelection == "scissors"){
+//         if (computerSelection == "rock") {
+//             alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+//         if (computerSelection == "paper") {
+//             alert(`You won! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+//         else {
+//             alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
+//         }
+
+
+
+
+
+//     } else {
+//         alert(`I'm not sure! You choose ${playerSelection} vs. ${computerSelection}`);
+//     }
+// }
+
+// playRound(playerSelection, computerSelection)
+
+
+// play a round of RPS based on the game(parameter) aka number of rounds
+function playGames(rounds) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameStart = prompt("Would you like to play a game of Rock, Paper, Scissors? (Y/N): ").toUpperCase();
+    let optionsRPS = ["Rock", "Paper", "Scissors"];
+
+    // loop rounds with the user input of "y" and cap set to rounds parameter 
+    while (gameStart == "Y" && playerScore < rounds && computerScore < rounds) {
+
+        // player selection
+        alert("You started a game!")
+        const playerSelection = prompt("Pick Rock, Paper, or Scissors: ").toLowerCase()
+        // computer selection
+        function getComputerChoice() {
+            return optionsRPS[(Math.floor(Math.random() * optionsRPS.length))];
         }
-        else if (computerSelection == "Scissors") {
-            alert(`You won! You choose ${playerSelection} vs. ${computerSelection}`);
-        } 
-        else {
-            alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
+        
+        const computerSelection = getComputerChoice().toLocaleLowerCase()
+
+        // Rock, Paper, Scissors game, Match player and computer selection and track score
+        function playRound(playerSelection, computerSelection) {
+
+            if (playerSelection == "rock") {
+                if (computerSelection == "paper") {
+                    computerScore++;
+                    alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+                else if (computerSelection == "scissors") {
+                    playerScore++;
+                    alert(`You won! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                } 
+                else {
+                    alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+            } else if (playerSelection == "paper"){
+                if (computerSelection == "rock") {
+                    playerScore++;
+                    alert(`You Won! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+                if (computerSelection == "scissors") {
+                    computerScore++;
+                    alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+                else {
+                    alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+            } else if (playerSelection == "scissors"){
+                if (computerSelection == "rock") {
+                    computerScore++;
+                    alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+                if (computerSelection == "paper") {
+                    playerScore++;
+                    alert(`You won! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }
+                else {
+                    alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+                }  
+            } else {
+                alert(`I'm not sure! You choose ${playerSelection} vs. ${computerSelection}\nYour Score: ${playerScore} Computer Score: ${computerScore}`);
+            }
         }
-    } else if (playerSelection == "Paper"){
-        if (computerSelection == "Rock") {
-            alert(`You Won! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-        if (computerSelection == "Scissors") {
-            alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-        else {
-            alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-    } else if (playerSelection == "Scissors"){
-        if (computerSelection == "Rock") {
-            alert(`You lost! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-        if (computerSelection == "Paper") {
-            alert(`You won! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-        else {
-            alert(`its a tie! You choose ${playerSelection} vs. ${computerSelection}`);
-        }
-    } else {
-        alert(`I'm not sure! You choose ${playerSelection} vs. ${computerSelection}`);
+        
+        playRound(playerSelection, computerSelection)
+        if (playerScore == rounds || computerScore == rounds)
+            alert(`Game over!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`)    
+
     }
+
+
 }
 
-playRound(playerSelection, computerSelection)
+playGames(2)
 
